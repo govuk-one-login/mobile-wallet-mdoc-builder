1f43873cab31c0b7b3ad7273035d7c8a9e6450e6
Shubhankar Choubey <shubhankar.choubey@digital.cabinet-office.gov.uk>
2026-06-18 18:57:01 +0100

    chore: updates to vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["src/**/*.test.ts", "tests/**/*.test-d.ts"],
+    include: ["src/**/*.test.ts"],
+    typecheck: {
+      enabled: true,
+      include: ["tests/**/*.test-d.ts"],
+    },
     environment: "node",
     coverage: {
       provider: "v8",
       reporter: ["text", "lcov"],
+      include: ["src/**/*.ts"],
+      exclude: ["src/**/*.test.ts"],
       thresholds: {
         lines: 100,
         functions: 100,
