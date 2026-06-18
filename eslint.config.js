e68a41ea2e579309baa2ad553cdf09477104d2b9
Shubhankar Choubey <shubhankar.choubey@digital.cabinet-office.gov.uk>
2026-06-18 18:13:38 +0100

    chore: add tsdown for the build (#8)
    * chore: add tsdown for the build
    
    * chore: pass_filenames for all hooks
    
    * chore: rework the PR workflows

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,18 @@
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig({
-  files: ["**/*.ts"],
-  extends: tseslint.configs.strictTypeChecked,
-  languageOptions: {
-    parserOptions: {
-      project: true,
-      tsconfigRootDir: import.meta.dirname,
+export default defineConfig([
+  {
+    ignores: ["dist/", "node_modules/"],
+  },
+  {
+    files: ["**/*.ts"],
+    extends: tseslint.configs.strictTypeChecked,
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
-  ignores: ["dist/", "node_modules/"],
-});
+]);
