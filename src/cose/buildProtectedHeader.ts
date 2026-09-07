f9726389dd50c4c86ae28218a1cf2c39f6597074
Niki Stefanopoulou <niki.stefanopoulou@digital.cabinet-office.gov.uk>
2026-09-07 17:11:18 +0100

    refactor: add back a comment

--- src/cose/buildProtectedHeader.ts
+++ src/cose/buildProtectedHeader.ts
@@ -1,5 +1,6 @@
 import { encode } from "../cbor";
 
+// COSE identifiers — see https://www.iana.org/assignments/cose/cose.xhtml
 const COSE_HEADER_ALG = 1; // alg header label
 const COSE_ALG_ES256 = -7; // ECDSA w/ SHA-256
 
