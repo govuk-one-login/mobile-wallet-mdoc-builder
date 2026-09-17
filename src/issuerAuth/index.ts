94d7f8e4cd11d983c1f5db63a8875c3684140325
Niki Stefanopoulou <niki.stefanopoulou@digital.cabinet-office.gov.uk>
2026-09-17 15:41:28 +0100

    chore: resolve PR comment for index exporting internal functions

--- src/issuerAuth/index.ts
+++ src/issuerAuth/index.ts
@@ -1,5 +1,2 @@
-export { buildProtectedHeader } from "./buildProtectedHeader.js";
-export { buildUnprotectedHeader } from "./buildUnprotectedHeader.js";
-export { buildToBeSigned } from "./buildToBeSigned.js";
 export { assembleIssuerAuth } from "./assembleIssuerAuth.js";
 export type { IssuerAuth } from "./assembleIssuerAuth.js";
